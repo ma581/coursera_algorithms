6a438a5b3d03af62fd4f71afdcedb5ded4b52596
maku <manojasho@gmail.com>
2020-03-05 22:13:19 +0000

    Recursively keeping count

--- test_inversions.py
+++ test_inversions.py
@@ -13,23 +13,27 @@ def count_inversions(input):
     else:
         left = input[0:length // 2]
         right = input[length // 2: length]
-        return count_inversions(left) + count_inversions(right) + count_split_inversions(input)
+        sorted, split_count = count_split_inversions(input, 0)
+        return count_inversions(left) + count_inversions(right) + split_count
 
 
-def count_split_inversions(input):
+def count_split_inversions(input, count):
     print(f'sorting  {input}')
     length = len(input)
     if length == 2:
         if input[0] <= input[1]:
-            return input
+            return input, 0
         else:
-            return [input[1], input[0]]
+            return [input[1], input[0]], 0
     else:
         left = input[0:length // 2]
         right = input[length // 2: length]
-        split_count, merged = merge(count_split_inversions(left), count_split_inversions(right))
-        print(f'merge: {merged}')
-        return split_count
+        left_sorted, left_count = count_split_inversions(left, count)
+        right_sorted, right_count = count_split_inversions(right, count)
+
+        merged, split_count = merge(left_sorted, right_sorted)
+        print(f'merged: {merged}, split_count: {split_count}, left_count: {left_count}, right_count: {right_count}')
+        return merged, split_count + left_count + right_count
 
 
 def merge(l, r):
@@ -54,7 +58,7 @@ def merge(l, r):
         else:
             merged[k:length_of_merged] = r
             break
-    return count, merged
+    return merged, count
 
 
 def test_should_find_one_inversion():
@@ -71,7 +75,17 @@ def test_should_find_split_inversion():
     input = [1, 4, 2, 3]  # 1 split inversion
     assert count_inversions(input) == 2
 
-def test_should_find_split_inversions():
-    input = [ 2, 4, 1, 3, 5, 6 ,7 ,8]  # 1 split inversion
-    assert count_inversions(input) == 3
 
+# def test_should_find_split_inversions():
+#     input = [ 2, 4, 1, 3, 5, 6 ,7 ,8]  # 1 split inversion
+#     assert count_inversions(input) == 3
+
+def test_should_sort_list():
+    input = [1, 2, 4, 3]
+    sorted, inversions = count_split_inversions(input, 0)
+    assert sorted == [1, 2, 3, 4]
+
+def test_should_sort_larger_list():
+    input = [1, 2, 4, 3, 8, 7, 9, 6]
+    sorted, inversions = count_split_inversions(input, 0)
+    assert sorted == [1, 2, 3, 4, 6, 7, 8 ,9]
